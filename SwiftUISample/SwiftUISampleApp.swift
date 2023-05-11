e2979fc8ab92300e1c179a85d3fdfa411262653c
Ankit Sharma <ankit.sharma10@publicissapient.com>
2023-05-11 14:58:18 +0530

    Initial Commit

---
+++ SwiftUISample/SwiftUISampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISampleApp.swift
+//  SwiftUISample
+//
+//  Created by Ankit Sharma on 11/05/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
